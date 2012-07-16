33e283b93e9e0abc1eebbc712d97b5c48e7e0187
Ashray Baruah <masj@emuparadise.org>
2012-07-16 18:15:38 -0400

    created tribal fusion async repo

---
+++ js/adloader.js
@@ -0,0 +1,6 @@
+/* Replace YOUR-ACCOUNT in the script tag below with your Tribalfusion account info */
+
+var tf_300 = '<scr'+'ipt type="text/javascript"><!--e9 = new Object();e9.size = "300x250";//--></scr'+'ipt><scr'+'ipt src="http://tags.expo9.exponential.com/tags/YOUR-ACCOUNT/ATF/tags.js"></scr'+'ipt>';
+$('#ad300').writeCapture().html(tf_300);
+
+
